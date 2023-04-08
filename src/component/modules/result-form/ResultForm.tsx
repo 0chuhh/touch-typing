5916478864c836f847c55e4766e0d342681e0c17
0chuhh <vvlasovdo@mail.ru>
2023-04-08 22:05:38 +0900

    statistic and chart

--- src/component/modules/result-form/ResultForm.tsx
+++ src/component/modules/result-form/ResultForm.tsx
@@ -1,17 +1,51 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import timeIcon from 'assets/time_icon.svg'
 import keyboardIcon from 'assets/keyboard.svg'
 import speedometerIcon from 'assets/speedometer.svg'
 import accuracyIcon from 'assets/accuracy.svg'
-import { Line } from 'react-chartjs-2';
-
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Filler,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2'
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Filler,
+);
 interface ResultFormProps {
-    time: { minutes: number, seconds: number }
+    time: string // mm:ss
     charCount: number
     charPerMin: number
-    accuracy?: number
+    accuracy: number
+    datasets: { charsPerMin: number[], accuracy: number[], seconds: number[] }
+    showContinueButton?: boolean
+    onContinueClick: (event: React.MouseEvent) => void
+    onResetClick: (event: React.MouseEvent) => void
+    onCompleteClick?: (event: React.MouseEvent) => void
 }
-const ResultForm: FC<ResultFormProps> = ({ time, charCount, charPerMin, accuracy }) => {
+const ResultForm: FC<ResultFormProps> = ({
+    time,
+    charCount,
+    charPerMin,
+    accuracy,
+    datasets,
+    showContinueButton: showContinueButton = true,
+    onContinueClick,
+    onResetClick,
+    onCompleteClick // this button is not used because it is intended to navigate to another page
+}) => {
+
+
+
     return (
         <div className="result-form">
             <h3>Результат</h3>
@@ -20,9 +54,7 @@ const ResultForm: FC<ResultFormProps> = ({ time, charCount, charPerMin, accuracy
                     <img width={42} src={timeIcon} alt="" />
                     <div>
                         <h6>Время</h6>
-                        <b>{time.minutes < 10 ? `0${time.minutes}` : time.minutes}
-                            :
-                            {time.seconds < 10 ? `0${time.seconds}` : time.seconds}
+                        <b>{time}
                         </b>
                     </div>
                 </div>
@@ -52,11 +84,49 @@ const ResultForm: FC<ResultFormProps> = ({ time, charCount, charPerMin, accuracy
                 </div>
 
             </div>
-            {/* <Line
-                options={...}
-                data={...}
-                {...props}
-            /> */}
+            <Line
+                style={{ maxHeight: '250px' }}
+                options={{
+
+                    elements: {
+                        point: {
+                            radius: 0
+                        }
+                    }
+
+                }}
+                data={{
+                    labels: datasets.seconds,
+                    datasets: [
+                        {
+                            data: datasets.charsPerMin,
+                            backgroundColor: 'rgba(55, 173, 221, 0.2)',
+                            borderColor: 'rgb(75, 192, 192)',
+                            tension: 0.4,
+                            fill: true
+                        },
+                        {
+                            data: datasets.accuracy,
+                            backgroundColor: 'rgba(194, 2, 2, 0.2)',
+                            borderColor: 'rgba(194, 2, 2,1)',
+                            tension: 0.4,
+                            fill: true
+                        },
+
+                    ]
+                }}
+
+            />
+            <div className="control-panel">
+                {/* this button is not used because it is intended to navigate to another page */}
+                <button onClick={onCompleteClick}>Завершить</button>
+                {/* -------------------------------------------------------------------------- */}
+                <button onClick={onResetClick}>Начать с начала</button>
+                {
+                    showContinueButton &&
+                    <button onClick={onContinueClick} className='second-btn'>Продолжить</button>
+                }
+            </div>
         </div>
     )
 }
